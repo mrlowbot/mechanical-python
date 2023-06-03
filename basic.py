57cc00af1196df7c3bbee9a7555de4dfa96d6a65
mrlowbot <karwanazar@icloud.com>
2023-06-03 21:34:51 +0200

    0.0.1

---
+++ basic.py
@@ -0,0 +1,12 @@
+######################################
+# Tokens for mechanical-python
+######################################
+
+class Token:
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+        
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
