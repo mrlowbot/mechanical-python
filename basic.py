d0b9508ae062556448ad7c1eb9e46d4267b22c27
mrlowbot <karwanazar@icloud.com>
2023-06-03 23:54:54 +0200

    0.0.2

--- basic.py
+++ basic.py
@@ -1,8 +1,61 @@
 ######################################
+# CONSTANTS: mechanical-python
+######################################
+DIGITS = '0123456789'
+
+
+######################################
+# POSITION: mechanical-python
+######################################
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.fn = fn
+        self.ftxt = ftxt
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        
+        if current_char == '\n':
+            self.ln += 1
+            self.col += 1
+            
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+
+######################################
+# ERRORS: mechanical-python
+######################################
+class Error:
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'\nError   => {self.error_name}\n'
+        result += f'Details => {self.details}\n'
+        result += f'File    => {self.pos_start.fn}\n'
+        result += f'Line    => {self.pos_start.ln + 1}\n'
+        return result
+
+    
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+
+######################################
 # TOKENS: mechanical-python
 # Hyper construction of advanced algorithms in combination of several different algorithms => https://www.youtube.com/watch?v=xvFZjo5PgG0
 ######################################
-
 TT_INT  = 'TT_INT'
 TT_FLOAT = 'TT_FLOAT'
 TT_PLUS = 'TT_PLUS'
@@ -14,7 +67,7 @@ TT_RPAREN = 'TT_RPAREN'
 
 
 class Token:
-    def __init__(self, type_, value):
+    def __init__(self, type_, value = None):
         self.type = type_
         self.value = value
         
@@ -26,9 +79,80 @@ class Token:
 ######################################
 # LEXER: mechanical-python
 ######################################
-
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+      
+    def make_tokens(self):
+        tokens = []
+        
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+                
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()    
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()                 
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()        
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()        
+            else:
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+           
+        
+        return tokens, None
+        
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
+        
+        
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else: 
+            return Token(TT_FLOAT, float(num_str))
+        
+        
+        
+        
+######################################
+# RUN: mechanical-python
+######################################
+def run(fn, text):
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    
+    return tokens, error
--- shell.py
+++ shell.py
@@ -1,3 +1,13 @@
+import basic
+
+
+
+
 while True:
     text = input('basic > ')
     print(text)
+    result, error = basic.run('<stdin>',text)
+    
+    
+    if error: print(error.as_string())
+    else: print(result)
